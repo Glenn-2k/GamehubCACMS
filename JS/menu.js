65b81a35151ebec1ac05e50ff802ccdaf09b2b44
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-01-08 14:14:47 +0100

    cahnged name of class

--- JS/menu.js
+++ JS/menu.js
@@ -1,14 +1,16 @@
-
-
-const checkbox = document.getElementById("menu-check");
+const checkbox = document.getElementById("menuCheck");
 const elementToToggle = document.querySelector(".menubar");
 
 export const checked = checkbox.addEventListener("click", () => {
-    if (checkbox.checked) {
-        elementToToggle.style.display = "block";
-        console.log("is checked");
-    } else {
-        elementToToggle.style.display = "none";
-        console.log("is not checked");
-    }
+  if (checkbox.checked) {
+    elementToToggle.style.display = "block";
+    console.log("is checked");
+  } else {
+    elementToToggle.style.display = "none";
+    console.log("is not checked");
+  }
 });
+
+if (event && !elementToToggle.contains(event.target) && menuCheck.checked) {
+  menuCheck.checked = false;
+}
